fb2d340d8d2ab1bb39f92c865e9e4d2676df27a2
andywarthol <andywarthol@gmail.com>
2015-06-17 15:04:07 -0700

    Adding culture template

---
+++ causes.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Causes
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="hero">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php
+		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
+		$url = $thumb['0'];
+	?>
+	<header style="background-image: url(<?=$url?>)">
+		<div class="overlay">
+			<h1 class="page-title text-center"><?php the_title(); ?></h1>
+		</div>
+	</header>
+	<?php endwhile; endif; ?>
+</section>
+<section class="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 col-md-12">
+				<div class="row">
+					<div class="col-sm-3 ">
+						<div class="well">
+							<h3>Our Company</h3>
+							<ul class="">
+								<?php wp_list_pages('sort_column=menu_order&exclude=44&title_li=&child_of=12&link_before=<i class="glyphicon glyphicon-chevron-right"></i>'); ?>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-9">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div class="row">
+							<div class="col-sm-12">
+								<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+									<?php the_content();  ?>
+
+								</article> <!-- end article -->
+							</div>
+						</div>
+						<?php endwhile; endif; ?>
+					</div>
+				</div>
+			</div>			
+		</div>
+	</div>
+</section>
+<script>
+	jQuery(document).ready(function($){
+		if (window.location.hash == "#vpclientservices") {
+			$('#field_oknffn').val('VP Client Services');
+		} else if (window.location.hash == "#sraccountmanager") {
+			$('#field_oknffn').val('Senior Account Manager');
+		}
+	});
+</script>
+<?php get_footer(); ?>
