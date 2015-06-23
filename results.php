0bef5be4df116116850ba428587d2eba1ff02fb8
andywarthol <andywarthol@gmail.com>
2015-06-23 10:36:19 -0700

    Mobile responsive

--- results.php
+++ results.php
@@ -23,11 +23,10 @@ Template Name: Results
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-10 col-lg-offset-1 col-md-12">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<div class="row">
 					<div class="col-sm-12 text-center">
-						<div id="filters" class="filters">
-							Filter by service: 
+						<div id="filters" class="filters hidden-xs">
+							<span class="hidden-xs">Filter by service: </span>
 							<button type="button" class="btn btn-default" data-filter="*">Show all</button>
 							<button type="button" class="btn btn-default" data-filter=".category-conversion">Conversion</button>
 							<button type="button" class="btn btn-default" data-filter=".category-design">Design</button>
@@ -43,43 +42,59 @@ Template Name: Results
 			</div>
 		</div>
 	</div>
-	<div class="container result-cards" style="margin-left: 30px">
-		<div class="col-lg-10 col-lg-offset-1 col-md-12">
-			<div class="row">
-				<?php
-					global $post;
-					$args = array( 'posts_per_page' => 24, 'category_name' => 'results', 'order' => 'ASC', 'orderby' => 'rand' );
-					$myposts = get_posts( $args );
-					foreach( $myposts as $post ) :  setup_postdata($post);
-				?>
-				<div <?php post_class( 'col-sm-3' ); ?>>
-					<article id="post-<?php the_ID(); ?>" class="card slim" role="article" itemscope itemtype="http://schema.org/BlogPosting" style="background-image: url('<?php the_field('company_logo'); ?>')">
-		        <h1><?php the_title(); ?></h1>
-						<em><?php the_field('the_figure'); ?></em>
-						<?php the_field('performance_metric'); ?><br/><?php the_field('time_frame'); ?>
-						<!-- <img src="<?php the_field('company_logo'); ?>" alt=""> -->
-		        <!-- <p class="flat text-center"><a href="<?php the_permalink(); ?>" class="btn btn-primary">Find out how <i class="glyphicon glyphicon-chevron-right"></i></a></p>-->
-					</article> <!-- end article -->
+	<div class="container result-cards">
+		<div class="row">
+			<div class="col-lg-10 col-lg-offset-1 col-sm-12">
+				<div class="row">
+					<?php
+						global $post;
+						$args = array( 'posts_per_page' => 24, 'category_name' => 'results', 'order' => 'ASC', 'orderby' => 'rand' );
+						$myposts = get_posts( $args );
+						foreach( $myposts as $post ) :  setup_postdata($post);
+					?>
+					<div <?php post_class( 'item col-xs-6 col-sm-3 col-md-3 col-lg-3' ); ?>>
+						<article id="post-<?php the_ID(); ?>" class="card slim" role="article" itemscope itemtype="http://schema.org/BlogPosting" style="background-image: url('<?php the_field('company_logo'); ?>')">
+							<h1><?php the_title(); ?></h1>
+							<em><?php the_field('the_figure'); ?></em>
+							<?php the_field('performance_metric'); ?><br/><?php the_field('time_frame'); ?>
+						</article> <!-- end article -->
+					</div>
+					<?php endforeach; ?>
+					<?php wp_reset_postdata(); ?>
 				</div>
-				<!-- Close the loop -->
-				<?php endforeach; ?>
-	      <?php wp_reset_postdata(); ?>
-			</div>
-			<?php endwhile; endif; ?>
-		</div><!-- col-md-10 -->
+			</div><!-- col-md-10 -->
+		</div>
 	</div>
-</div>
+	<div class="container mobile-results visible-xs">
+		<div class="row">
+			<?php
+				global $post;
+				$args = array( 'posts_per_page' => 24, 'category_name' => 'results', 'order' => 'ASC', 'orderby' => 'rand' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) :  setup_postdata($post);
+			?>
+			<div <?php post_class( 'item col-xs-6 col-sm-3 col-md-3 col-lg-3' ); ?>>
+				<article id="post-<?php the_ID(); ?>" class="card slim post-<?php the_ID(); ?>" role="article" itemscope itemtype="http://schema.org/BlogPosting" style="background-image: url('<?php the_field('company_logo'); ?>')">
+					<h1><?php the_title(); ?></h1>
+					<em><?php the_field('the_figure'); ?></em>
+					<?php the_field('performance_metric'); ?><br/><?php the_field('time_frame'); ?>
+				</article> <!-- end article -->
+			</div>
+			<?php endforeach; ?>
+			<?php wp_reset_postdata(); ?>
+		</div>
+	</div>
 </section>
 <script src="<?php bloginfo('template_directory'); ?>/library/js/isotope.min.js"></script>
 <script>
 	jQuery(document).ready(function($){
 		$('.result-cards').isotope({
-		  itemSelector: '.col-sm-3',
-		  layoutMode: 'masonry'
+			itemSelector: '.col-sm-3',
+			layoutMode: 'masonry'
 		});
 		$('#filters').on( 'click', 'button', function() {
-		  var filterValue = $(this).attr('data-filter');
-		  $('.result-cards').isotope({ filter: filterValue });
+			var filterValue = $(this).attr('data-filter');
+			$('.result-cards').isotope({ filter: filterValue });
 		});
 	});
 </script>
