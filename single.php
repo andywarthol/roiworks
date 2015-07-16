38dbeb53d140e64b4d13aa16dccc90e4fa2c5168
andywarthol <andywarthol@gmail.com>
2015-07-16 12:23:47 -0700

    Additional mobile blog polish

--- single.php
+++ single.php
@@ -15,7 +15,7 @@
 							<div class="featured-image" style="background-image: url(<?=$url?>)">
 							</div>
 							<div class="card-content">
-								<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+								<h1><?php the_title(); ?></h1>
 								<div class="byline vcard"><?php
 										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', ') );
 									?></div>
